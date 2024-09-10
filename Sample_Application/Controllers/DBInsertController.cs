417831e0f14f576d0d7f61a5d12bb789c0e17240
nizam <nizambinnazar@gmail.com>
2024-09-10 10:40:04 +0530

    Add project files.

---
+++ Sample_Application/Controllers/DBInsertController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample_Application.Controllers
+{
+    public class DBInsertController : Controller
+    {
+        // GET: DBInsert
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sample_Application/Models/stinsert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sample_Application.Models
+{
+    public class stinsert
+    {
+        public string Sname { get; set; }
+        public int Sage { get; set; }
+        public string Saddress { get; set; }
+    }
+}
