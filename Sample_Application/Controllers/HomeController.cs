050d57987ad81fd54ae9cc7a16dc193ef0169501
nizam <nizambinnazar@gmail.com>
2024-09-10 10:53:07 +0530

    updated

--- Sample_Application/Controllers/HomeController.cs
+++ Sample_Application/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Sample_Application.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page here.";
 
             return View();
         }
